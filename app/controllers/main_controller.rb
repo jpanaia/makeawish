f3efc1d8bce5e9ce20b258d7ce0ee2fe78dcd693
Jaime <jpanaia@gmail.com>
2014-12-15 01:16:36 -0500

    added js

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,6 @@
 class MainController < ApplicationController
   def index
   	@current_time = Time.now.strftime("%I:%M")
-    @wish_times = ['12:55', '1:11', '2:22', '3:33', '4:44', '5:55', '10:10', '11:11', '12:12']
-  
+    @wish_times = ['01:11', '02:22', '03:33', '04:44', '05:55', '10:10', '11:11', '12:12']
   end
 end
