932739e2826af75368420f8af2288bd55695f378
Jaime <jpanaia@gmail.com>
2014-12-15 00:57:44 -0500

    added logic around wish button

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,7 @@
 class MainController < ApplicationController
   def index
+  	@current_time = Time.now.strftime("%I:%M")
+    @wish_times = ['12:55', '1:11', '2:22', '3:33', '4:44', '5:55', '10:10', '11:11', '12:12']
+  
   end
 end
