218052efa8d507dc7e3d2d810bf500e785e9e012
Jaime <jpanaia@gmail.com>
2014-12-15 18:30:32 -0500

    calculated closest wish time. hopefully

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,15 @@
 class MainController < ApplicationController
+include MainHelper
   def index
-  	@current_time = Time.now.strftime("%I:%M")
-    @wish_times = ['01:11', '02:22', '03:33', '04:44', '05:55', '10:10', '11:11', '12:12']
+  	@current_time = Time.now
+    wish_times 
+    @wish_times = [@one_11, @two_22, @three_33, @four_44, @five_55, @ten_10, @eleven_11, @twelve_12p, @one_11p, @two_22p, @three_33p, @four_44p, @five_55p, @ten_10p, @eleven_11p, @twelve_12]
+    # @wish_times = ['01:11', '02:22', '03:33', '04:44', '05:55', '10:10', '11:11', '12:12']
     @activities = Activity.all
-    @duration = 360
+
+    minutes_until_wish_times
+    closest_wish_time
   end
+
+
 end
--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,2 +1,67 @@
 module MainHelper
-end
+
+  def wish_times 
+	@one_11 = Chronic.parse('1:11am')
+    @one_11p = Chronic.parse('1:11pm')
+
+    @two_22 = Chronic.parse('2:22am')
+    @two_22p = Chronic.parse('2:22pm')
+
+    @three_33 = Chronic.parse('3:33am')
+    @three_33p = Chronic.parse('3:33pm')
+
+    @four_44 = Chronic.parse('4:44am')
+    @four_44p = Chronic.parse('4:44pm')
+
+    @five_55 = Chronic.parse('5:55am')
+    @five_55p = Chronic.parse('5:55pm')
+
+    @ten_10 = Chronic.parse('10:10am')
+    @ten_10p = Chronic.parse('10:10pm')
+
+    @eleven_11 = Chronic.parse('11:11am')
+    @eleven_11p = Chronic.parse('11:11pm')
+
+    @twelve_12 = Chronic.parse('12:12am')
+    @twelve_12p = Chronic.parse('12:12pm')
+  end
+
+  def minutes_until_wish_times
+  	@durations = []
+
+  	@wish_times.each do |time| 
+      @duration = time - Time.now
+	  @duration = (@duration / 60).to_i  #time in minutes
+	  @durations.push([@duration,time])
+	   # puts "************"
+    #    puts @duration
+    #    puts "************"
+
+    end
+       # puts "************"
+       # print @durations
+       # puts "************"
+  end #end of minutes_until_wish_times
+
+  def closest_wish_time
+  	@pos_durations = []
+
+  	@durations.each do |duration|
+	  	if duration[0] > 0
+	     @pos_durations.push([duration[0],duration[1]])
+	   #   puts "************"
+	  	# puts duration[0]
+	  	# puts "************"
+	  	end
+     end
+     
+     @closest_pair = @pos_durations.min
+     @closest_wish_time = @closest_pair[1]
+     @closest_duration = @closest_pair[0]
+	   puts "************"
+       print @closest_duration
+       print @closest_wish_time
+       puts "************"
+  end # end of closest_wish_time
+
+end # end of helper
