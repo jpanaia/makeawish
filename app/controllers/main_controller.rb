74657df0de7f741dd289ea3efb6800e7459ca054
Jaime <jpanaia@gmail.com>
2014-12-15 01:28:44 -0500

    pulled activities onto homepage

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,5 +2,7 @@ class MainController < ApplicationController
   def index
   	@current_time = Time.now.strftime("%I:%M")
     @wish_times = ['01:11', '02:22', '03:33', '04:44', '05:55', '10:10', '11:11', '12:12']
+    @activities = Activity.all
+    @duration = 360
   end
 end
