d3dd618f3eb7c56b637968cada43feadcc69f5a2
Courtney Rodi <courtney76@gmail.com>
2014-12-16 00:38:45 -0500

    fixed seed to only include activities

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,4 @@
-User.create!([
-  {email: "wish@wish.com", encrypted_password: "$2a$10$kcsmhOFVcyEAgXtRW0/DH.p1kg6/yhL0ICEssAAfDE6/V0L6nF9TC", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2014-12-16 05:11:15", last_sign_in_at: "2014-12-16 05:11:15", current_sign_in_ip: "::1", last_sign_in_ip: "::1", admin: nil}
-])
+
 Activity.create!([
   {task: "Boil an Egg", ittake: "2000-01-01 00:10:00"},
   {task: "Take a Shower", ittake: "2000-01-01 00:08:00"},
